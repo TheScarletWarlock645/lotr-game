4af6d857cbcc0ccdff303063b2672f3034ab3b03
Uranium Hoarder <angeloreid24@gmail.com>
2025-11-24 17:39:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,105 +3,89 @@
 #License: GNU GPLv3
 #Year: 2025
 
-import json
+import functions as funcs
 import os
-import sys
-from functools import wraps
 
-error_codes = {
-    1: "Unkown or Unexpected error",
-    2: "JSON decoder error",
-    3: "File not found"
-}
+save_data_file = "./data/save_data.json"
+game_data_file =  "./data/game_data.json"
 
-def error_listener(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
+try:
+    result, game_data = funcs.load_data(game_data_file) # Loads game data from file
+    print("\n** WELCOME TO MIDDLE EARTH! **\n")
 
-        if isinstance(result, int) and result in error_codes:
-            print(f"ERROR: {result}: {error_codes[result]}")
-            print(f"Error occured in function: {func.__name__}")
-            sys.exit(result)
-        
-        return result
-    return wrapper
+    start_action_choices = ["New save", "Load save", "Exit"]
 
-# Loads game data from game data file (default: game_data.json)
-@error_listener
-def load_game_data(path):
-    file_path = os.path.abspath(path)
-    try:
-        with open(file_path, "r") as data:
-            game_data = json.load(data)
+    for i, name in enumerate(start_action_choices, 1):
+        print(f"{i}. {name}")
 
-        return 0, game_data
-    except FileNotFoundError:
-        return 3, None
-    except json.JSONDecodeError:
-        return 2, None
-    except Exception:
-        return 1, None
-
-# Creates a new character/save in the save data file (default: save_data.json)
-@error_listener
-def new_save(path, fname, lname, race, game_section, gender=None):
-    file_path = os.path.abspath(path)
-    char_name = f"{fname} {lname}".title()
-
-    try:
-        with open(file_path, "r") as f:
-            content_check = f.read()
-        
-        if not content_check:
-            new_save = {
-                char_name: {
-                    "fname": fname,
-                    "lname": lname,
-                    "race": race,
-                    "gender": gender,
-                    "game_section": game_section
-                }
-            }
-
-            try:
-                with open(file_path, "w") as save_data:
-                    json.dump(new_save, save_data, indent=4)
-            except FileNotFoundError:
-                return 3, file_path, None
-            except Exception as e:
-                return 1, None, e
-
-        new_save = {
-            char_name: {
-                "fname": fname,
-                "lname": lname,
-                "race": race,
-                "gender": gender,
-                "game_section": game_section
-            }
-        }
-
+    while True: #Error loop for invalid action selections
         try:
-            with open(file_path, "r") as all_saves:
-                all_saves_loaded = json.load(all_saves)
+            start_action = int(input("Choose an action (number): "))
+            break
+        except ValueError:
+            print("ERROR: Please enter a valid number to start!")
+            continue
 
-            all_saves_loaded.append(new_save)
-            
-        except FileNotFoundError:
-            return 3
-        except json.JSONDecodeError as e:
-            return 2
-        except Exception as e:
-            return 1
+    # New Save
+    if start_action == 1:
+        save_data = funcs.load_data(save_data_file)
+
+        char_id = len(save_data) - 1
+
+        fname = input("\nEnter your character's first name: ")
+        lname = input("\nEnter your character's last name: ")
+        print()
         
-        try:
-            with open(file_path, "w") as f:
-                content_check = f.read()
+        race_options = ["Dwarf", "Elf", "Hobbit", "Human", "Orc"]
+        for i, name in enumerate(race_options, 1):
+            print(f"{i}. {name}")
+        race = int(input("Please choose a race (number): "))
 
-                if not content_check:
-                    
-                else:
-                    json.dump(all_saves_loaded, f, indent=4)
+        print()
+        char_class_options = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Rogue', 'Wizard']
+        for i, name in enumerate(char_class_options, 1):
+            print(f"{i}. {name}")
+        char_class = int(input("Please choose a class (number): "))
 
-     
+        gender = input("\nChoose a gender (optional): ")
+        if gender == "":
+            gender = None
+
+        if game_data["unlocked_areas"] == None:
+            game_section = 1
+        else:
+            while True:
+                game_section = int(input(f"\nChoose starting section (1-{game_data['unlocked_areas']}): "))
+
+                if game_section > game_data["unlocked_areas"]:
+                    print("ERROR: You haven't unlocked that level!")
+                    continue
+                elif game_section < 1:
+                    print("ERROR: Invalid level!")
+                    continue
+
+                break
+
+        new_save = funcs.new_save(save_data_file, char_id ,fname, lname, race, char_class, game_section, gender)
+        if new_save == 0:
+            start_now_query = input("\nSuccessfully made new save! Would you like to start now? (Y/n) ").lower()
+            if start_now_query == "" or start_now_query == "y":
+                funcs.load_save(char_id)
+            else:
+                exit("\nBye :)\n")
+        
+    # Load save
+    elif start_action == 2:
+        all_saves = funcs.load_data(save_data_file)
+        for i , name in enumerate(all_saves.keys(), 1):
+            print(f"{i}. {name}")
+
+##CONTINUE HERE, FINISH MAKING SAVE LOADING
+#TODO
+
+    # Exit game
+    elif start_action == 3:
+        exit("\nBye! Come back soon!\n")
+
+except KeyboardInterrupt:
+    exit("\n\nBye :)\n")
