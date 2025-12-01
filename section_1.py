d242d63fff7a8e6ef7ab4394bb5d34f8b3f2cb21
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-12-01 09:35:06 -0500

    updated section_1.py

--- section_1.py
+++ section_1.py
@@ -12,9 +12,17 @@ game_data_file =  "./data/game_data.json"
 
 # Loading character data
 char_id = os.environ.get('LOADED_SAVE')
+
 _, fname, lname, race, char_class, gender, game_section = funcs.load_save_data(char_id, save_data_file)
+
 _, pers_pronoun, poss_pronoun_1, poss_pronoun_2, obj_pronoun = funcs.pronouns(gender)
+
 full_name = f"{fname} {lname}".title()
+
 print("="*100)
-print(f"\ncharacter id: {char_id}\npronouns: {pers_pronoun}, {poss_pronoun_1}, {poss_pronoun_2}, {obj_pronoun}\
-      \nfirst name: {fname}\nlast name: {lname}\nrace: {race}\nclass: {char_class}")
+
+
+time.sleep(2)
+with open("./images/tower.txt", "r") as f:
+      print(f.read())
+print("")
